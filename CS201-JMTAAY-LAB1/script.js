b75542d283eb8a92801a42fd1476f8334a798d5f
TAAY,Janeece Mae <147222812+janeecetaay@users.noreply.github.com>
2023-10-09 10:01:24 +0800

    Add files via upload

---
+++ CS201-JMTAAY-LAB1/script.js
@@ -0,0 +1,9 @@
+let firstFavNum = 112;
+let secondFavNum = 120;
+console.log(firstFavNum+secondFavNum);
+console.log(firstFavNum-secondFavNum);
+console.log(firstFavNum*secondFavNum);
+console.log(firstFavNum/secondFavNum);
+console.log(firstFavNum%secondFavNum);
+let power = Math.pow(firstFavNum);
+console.log(power);
